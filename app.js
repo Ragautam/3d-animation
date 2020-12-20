a63894bae8f4c74b72496c22cc3c0f237d110c9a
Ragautam <gautamkumarradadiya009@gmail.com>
2020-12-20 12:27:10 +0530

    Add 3D animation

---
+++ app.js
@@ -0,0 +1,34 @@
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+const sneaker = document.querySelector(".sneaker img")
+const title = document.querySelector(".title");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+const purchase = document.querySelector(".purchase");
+
+container.addEventListener("mousemove", (e) => {
+  const xAxis = (window.innerWidth/2 - e.pageX)/25;
+  const yAxis = (window.innerHeight/2 - e.pageY)/25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
+});
+
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+
+  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  title.style.transform = "translateZ(150px)";
+  description.style.transform = "translateZ(125px)";
+  sizes.style.transform = "translateZ(100px)";
+  purchase.style.transform = "translateZ(75px)";
+});
+
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = "rotateY(0deg) rotateX(0deg)";
+
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  title.style.transform = "translateZ(0px)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
+});
