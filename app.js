1af1cfac43c72acdb88e12d406fda0ab1c5ba6ee
Ragautam <gautamkumarradadiya009@gmail.com>
2020-12-20 12:47:28 +0530

    add touch event

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const description = document.querySelector(".info h3");
 const sizes = document.querySelector(".sizes");
 const purchase = document.querySelector(".purchase");
 
-container.addEventListener("mousemove", (e) => {
+const onMouseMove = (e) => {
   const xAxis = (window.innerWidth/2 - e.pageX)/25;
   const yAxis = (window.innerHeight/2 - e.pageY)/25;
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
-});
+};
 
-container.addEventListener("mouseenter", (e) => {
+const onMouseEnter = (e) => {
   card.style.transition = "none";
 
   sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
@@ -20,9 +20,9 @@ container.addEventListener("mouseenter", (e) => {
   description.style.transform = "translateZ(125px)";
   sizes.style.transform = "translateZ(100px)";
   purchase.style.transform = "translateZ(75px)";
-});
+};
 
-container.addEventListener("mouseleave", (e) => {
+const onMouseLeave = (e) => {
   card.style.transition = "all 0.5s ease";
   card.style.transform = "rotateY(0deg) rotateX(0deg)";
 
@@ -31,4 +31,13 @@ container.addEventListener("mouseleave", (e) => {
   description.style.transform = "translateZ(0px)";
   sizes.style.transform = "translateZ(0px)";
   purchase.style.transform = "translateZ(0px)";
-});
+};
+
+container.addEventListener("mousemove", onMouseMove);
+container.addEventListener("touchmove", onMouseMove);
+
+container.addEventListener("mouseenter", onMouseEnter);
+container.addEventListener("touchstart", onMouseEnter);
+
+container.addEventListener("mouseleave", onMouseLeave);
+container.addEventListener("touchend", onMouseEnter);
